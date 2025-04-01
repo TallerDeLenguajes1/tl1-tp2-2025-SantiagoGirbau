d9922dc0f1bfdd3c36d83d4ae641f32b9870744b
SantiagoGirbau <santiagogirbau@gmail.com>
2025-04-01 13:57:26 -0300

    Commit grande, apartados 1, 2 y 3 andando

---
+++ tp2_4.c
@@ -0,0 +1,72 @@
+#include "stdlib.h"
+#include "stdio.h"
+#include "math.h"
+#include "time.h"
+
+    struct compu {
+ int velocidad; // Velocidad de procesamiento en GHz (valor entre 1 y 3)
+ int anio; // Año de fabricación (valor entre 2015 y 2024)
+ int cantidad_nucleos; // Cantidad de núcleos (valor entre 1 y 8)
+ char *tipo_cpu; // Tipo de procesador (apuntador a cadena de caracteres)
+}typedef compu;
+
+compu generarCompu(compu pc1);
+void listarPCs (compu pc[], int i);
+
+int main()
+{
+    srand(time(NULL));
+    
+    compu pc[5];
+    int trigger, cantidad;
+    for (int i = 0; i < 5; i++)
+    {
+        pc[i] = generarCompu(pc[i]); 
+        cantidad = i;
+    }
+    printf("Bienvenidos al himalaya:\n\n Escriba el numero correspondiente a la opcion que se desee realizar:\n 1 listar PCs \n 2 mostrar PC mas vieja \n 3 mostrar PC mas veloz\n");
+    scanf("%i", &trigger);
+
+    switch (trigger)
+    {
+        case 1:
+            listarPCs(pc, cantidad);
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        default:
+            break;
+    }
+
+
+
+
+    
+
+    return 0;
+}
+
+compu generarCompu(compu pc1){
+    char tipos[6][10] = {"Intel", "Ryzen", "Celeron", "Athlon", "Core", "Pentium"};
+    pc1.velocidad= 1+rand()%3;
+    pc1.anio= 2015+rand()%10;
+    pc1.cantidad_nucleos=1+rand()%8;
+    pc1.tipo_cpu=tipos[1+rand()%6];
+    return(pc1);
+}
+
+void listarPCs (compu pc[], int cantidad){
+
+    for (short i = 0; i < cantidad; i++)
+    {
+        printf("\n  -Compu numero %i \n", (i+1));
+        printf("Velocidad: %i GHz\n", pc[i].velocidad);
+        printf("Anio: %i\n", pc[i].anio);
+        printf("Cantidad de nucleos: %i\n", pc[i].cantidad_nucleos);
+        printf("Tipo de la CPU: %i\n", pc[i].tipo_cpu);
+    }
+}
