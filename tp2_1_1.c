039295e9b41e97eeb00c637581ff683f58b71634
SantiagoGirbau <santiagogirbau@gmail.com>
2025-04-01 11:38:59 -0300

    inicial

---
+++ tp2_1_1.c
@@ -0,0 +1,17 @@
+// codigo a completar
+
+int main(int argc, char const *argv[])
+{
+        /* code */
+    #define N 20
+    int i;
+    double vt[N];
+
+    for (i = 0; i < N; i++)
+    {
+        vt[i] = 1 + rand() % 100;
+        printf("%f", vt[i]);
+    }
+
+        return 0;
+}
